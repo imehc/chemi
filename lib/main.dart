54fcdbe7abc8ed294d27f62da9e4996a9cacb7b5
chemi <time19@yeah.net>
2022-04-14 15:48:10 +0800

    flutter_neumorphic 插件

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,119 @@
-import 'package:flutter/material.dart';
+import 'package:flutter_blog/widgets/page_home.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const NeumorphicApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter",
+      themeMode: ThemeMode.light,
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xffffffff),
+        lightSource: LightSource.topLeft,
+        depth: 10,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: NeumorphicThemeData(
+        baseColor: Color(0xff3e3e3e),
+        lightSource: LightSource.topLeft,
+        depth: 6,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      floatingActionButton: NeumorphicFloatingActionButton(
+        child: const Icon(
+          Icons.add,
+          size: 30,
+        ),
+        onPressed: () {},
       ),
+      backgroundColor: NeumorphicTheme.baseColor(context),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            NeumorphicButton(
+              onPressed: () {
+                print("onClick");
+              },
+              style: const NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape: NeumorphicBoxShape.circle()),
+              padding: const EdgeInsets.all(12.0),
+              child: Icon(
+                Icons.favorite_border,
+                color: _iconsColor(context),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            NeumorphicButton(
+              margin: const EdgeInsets.only(top: 12),
+              onPressed: () {
+                NeumorphicTheme.of(context)!.themeMode =
+                    NeumorphicTheme.isUsingDark(context)
+                        ? ThemeMode.light
+                        : ThemeMode.dark;
+              },
+              style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(8))),
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "Toggle Theme",
+                style: TextStyle(color: _textColor(context)),
+              ),
+            ),
+            NeumorphicButton(
+              margin: const EdgeInsets.only(top: 12),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return const FullSampleHomePage();
+                }));
+              },
+              style: NeumorphicStyle(
+                  shape: NeumorphicShape.flat,
+                  boxShape:
+                      NeumorphicBoxShape.roundRect(BorderRadius.circular(8))),
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "Go to full sample",
+                style: TextStyle(color: _textColor(context)),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+Color? _iconsColor(BuildContext context) {
+  final theme = NeumorphicTheme.of(context);
+  if (theme!.isUsingDark) {
+    return theme.current!.accentColor;
+  } else {
+    return null;
+  }
+}
+
+Color _textColor(BuildContext context) {
+  if (NeumorphicTheme.isUsingDark(context)) {
+    return Colors.white;
+  } else {
+    return Colors.black;
   }
 }
