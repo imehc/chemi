a0f3e2a8398a029f4890b14eac080939c496425f
chemi <time19@yeah.net>
2022-05-17 16:34:43 +0800

    add flutter_local_notifications and fonts

---
+++ lib/utils/local_notification.dart
@@ -0,0 +1,48 @@
+import 'dart:developer';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+// 来源： https://blog.csdn.net/qq_41806966/article/details/121984458
+class LocalNotification {
+  static final FlutterLocalNotificationsPlugin _np =
+      FlutterLocalNotificationsPlugin();
+
+  /// 是否初始化了
+  static var _isInit = false;
+
+  /// 初始化
+  static Future<void> _init() async {
+    if (_isInit) return;
+    _isInit = true;
+
+    var android = const AndroidInitializationSettings("@mipmap/ic_launcher");
+    var iOS = const IOSInitializationSettings();
+
+    try {
+      await _np.initialize(InitializationSettings(android: android, iOS: iOS));
+    } catch (e) {
+      log("LocalNotification init error: $e");
+    }
+  }
+
+  static void send(String title, String body) async {
+    // 初始化
+    await _init();
+
+    // 构建描述
+    var androidDetails = const AndroidNotificationDetails('id描述', '名称描述',
+        importance: Importance.max, priority: Priority.high);
+    var iosDetails = const IOSNotificationDetails();
+    var details = NotificationDetails(android: androidDetails, iOS: iosDetails);
+
+    // ⚠️:iOS Notifications are not displayed if the app is in the foreground
+
+    // 显示通知, 第一个参数是id,id如果一致则会覆盖之前的通知
+    try {
+      _np.show(
+          DateTime.now().millisecondsSinceEpoch >> 10, title, body, details);
+    } catch (e) {
+      log("LocalNotification send error: $e");
+    }
+  }
+}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,5 @@
 export 'creame_scan_code.dart';
 export 'handle_file_to_local.dart';
 export 'local_images_scan_code.dart';
+export 'local_notification.dart';
 export 'open_url.dart';
