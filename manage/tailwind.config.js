727eabc9535195cbd919d5097808040c2eebe8d2
chemi <time19@yeah.net>
2022-07-15 15:44:21 +0800

    feat(manage): 引入tailwindcss

---
+++ manage/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
