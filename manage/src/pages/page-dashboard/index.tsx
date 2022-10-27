33cbba659a14f52e4b4ba0132b8998d72fdb674f
chemi <time19@yeah.net>
2022-10-27 09:31:34 +0800

    fix(manage): init template

--- manage/src/pages/page-dashboard/index.tsx
+++ manage/src/pages/page-dashboard/index.tsx
@@ -1,60 +1,10 @@
-import React, { useCallback, useEffect } from 'react';
-import DB from '~/database/indexed_db';
-
-import { useAccessToken, useGlobalContext } from '~/globalContext';
+import React from 'react';
 
 export const PageDashboard: React.FC = () => {
-  const { setAccessToken, removeAccessToken } = useGlobalContext();
-  const accessToken = useAccessToken();
-
-  const initDb = useCallback(async () => {
-    const airbnbDB = new DB('storeName');
-    // IndexedDB
-    const db = await airbnbDB.openStore({
-      // 数据表的名字
-      web_user: {
-        // 主键
-        keyPath: 'userId',
-        // 索引列表
-        indexs: ['mobile', 'password', 'status'],
-      },
-    });
-    await airbnbDB.updateItem('web_user', { name: '张三', age: 30 });
-    const res = await airbnbDB.getAllItem('web_user');
-    console.log(res, 'res....');
-  }, []);
-
-  useEffect(() => {
-    initDb();
-  }, []);
-
+ 
   return (
     <React.Fragment>
-      <button
-        className="px-[5px] border-[1px] rounded-[5px] border-solid border-gray-300 text-[#202324]"
-        onClick={() => {
-          setAccessToken('new token');
-        }}
-      >
-        更新token
-      </button>
-      <button
-        className="px-[5px] border-[1px] rounded-[5px] border-solid border-gray-300 text-[#202324]"
-        onClick={() => {
-          removeAccessToken();
-        }}
-      >
-        删除token
-      </button>
-      <button
-        className="px-[5px] border-[1px] rounded-[5px] border-solid border-gray-300 text-[#202324] mb-5"
-        onClick={() => {
-          console.log(accessToken, 'globalAccessToken');
-        }}
-      >
-        查看token
-      </button>
-      <hr />
+     21
     </React.Fragment>
   );
 };
--- manage/src/router/BaseRouter.tsx
+++ manage/src/router/BaseRouter.tsx
@@ -1,22 +1,14 @@
-import React, { useCallback, useMemo } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { GlobalContextProvider } from '~/globalContext';
-import type { GlobalContextProps } from '~/globalContext';
 import { PageBasic } from '~/pages';
-import { useStorage } from '~/hooks';
 import AuthRouter from './AuthRouter';
-import { CustomDeleteDialogProvider } from '~/components';
 
 const BaseRouter: React.FC = () => {
   return (
-    <CustomDeleteDialogProvider>
-      <GlobalContextProvider>
-        <Routes>
-          <Route path="login" element={<PageBasic />} />
-          <Route path="*" element={<AuthRouter />} />
-        </Routes>
-      </GlobalContextProvider>
-      </CustomDeleteDialogProvider>
+    <Routes>
+      <Route path="login" element={<PageBasic />} />
+      <Route path="*" element={<AuthRouter />} />
+    </Routes>
   );
 };
 export default BaseRouter;
