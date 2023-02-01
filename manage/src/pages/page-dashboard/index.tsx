a1e55a15c39b9dea062c7a7df9b18dee063bb480
chemi <time19@yeah.net>
2023-02-01 14:09:58 +0800

    refactor(manage): 替换包管理器为pnpm

--- manage/src/pages/page-dashboard/index.tsx
+++ manage/src/pages/page-dashboard/index.tsx
@@ -3,7 +3,7 @@ import React, { type FC } from 'react';
 export const PageDashboard: FC = () => {
   return (
     <React.Fragment>
-      <div className="inline-block text-[36px] text-blue-300 i-mingcute:cellphone-line" />
+      <div className="inline-block text-[36px] i-twemoji-frog m-10 animate-bounce-alt animate-count-infinite animate-duration-1s" />
     </React.Fragment>
   );
 };
