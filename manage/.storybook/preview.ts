61b622fce2bdcbaa551ab4e0d229263ad69bc2a5
chemi <time19@yeah.net>
2023-02-07 16:45:02 +0800

    fix(manage): init storybook

---
+++ manage/.storybook/preview.ts
@@ -0,0 +1,13 @@
+import '@unocss/reset/tailwind.css';
+import "../src/style.css";
+import 'uno.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
