2514388c796290de2a7b2d0ee1456298635c5a68
chemi <time19@yeah.net>
2023-02-23 16:26:44 +0800

    fix(manage): 升级依赖

--- manage/vite.config.ts
+++ manage/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath, URL } from 'url';
-import { resolve } from 'path';
 import eslintPlugin from 'vite-plugin-eslint';
 import unocss from 'unocss/vite';
 
@@ -12,19 +11,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 6012,
-    proxy: {
-      '/apis': {
-        target: 'https://v1.hitokoto.cn',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/apis/, ''), // 不可以省略rewrite
-      },
-    },
   },
 
   resolve: {
     alias: {
-      // 设置别名后如果是typescript则必须在tsconfig.json中配置baseUrl和paths，否者ts类型会报错
-      '#': resolve(__dirname, 'src'),
       '~': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
