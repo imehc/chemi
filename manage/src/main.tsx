fc37d0ca9f9b9570b32d2d9c7b9bf49d5a614306
chemi <time19@yeah.net>
2023-03-31 18:03:26 +0800

    fix(manage): 升级依赖

--- manage/src/main.tsx
+++ manage/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import BaseRouter from '~/router/BaseRouter';
 import '@unocss/reset/tailwind.css';
-import './style.css';
-import 'uno.css';
+import './index.css';
+import 'virtual:uno.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
