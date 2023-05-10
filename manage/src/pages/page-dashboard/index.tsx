b1180143cd1aafc91fd5bbe066481eefaa407e15
chemi <time19@yeah.net>
2023-05-10 09:40:37 +0800

    feat(manage): 轴端点添加文字

--- manage/src/pages/page-dashboard/index.tsx
+++ manage/src/pages/page-dashboard/index.tsx
@@ -1,28 +1,11 @@
 import React, { type FC } from 'react';
 import { ThreeDemo01 } from '~/components';
-import { useSpring, animated } from '@react-spring/web';
+import { ThreeAxesHelper } from '~/components/componet-axes-helper';
 
 export const PageDashboard: FC = () => {
-  const [props, api] = useSpring(
-    () => ({
-      from: { opacity: 0 },
-      to: { opacity: 1 },
-    }),
-    []
-  );
-
   return (
     <React.Fragment>
-      {/* <div
-        className="animate-bounce-alt animate-duration-1s animate-count-infinite i-twemoji-frog"
-        m="10"
-        text="36px"
-        display="inline-block"
-      /> */}
-      {/* <ThreeDemo01 /> */}
-      <div>
-        <animated.div style={props}>Hello World</animated.div>
-      </div>
+      <ThreeAxesHelper />
     </React.Fragment>
   );
 };
