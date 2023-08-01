9e78148a23c06eb42026477cb301290c24920ae1
chemi <time19@yeah.net>
2023-08-01 15:06:37 +0800

    feat(manage): 模型demo

--- manage/src/components/component-preview-model/index.tsx
+++ manage/src/components/component-preview-model/index.tsx
@@ -1,8 +1,9 @@
 import { Html, OrbitControls, Sky } from '@react-three/drei';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { Suspense } from 'react';
-import { MOUSE } from 'three';
+import { Canvas, useLoader, useThree } from '@react-three/fiber';
+import { Suspense, useEffect, useMemo, useRef } from 'react';
+import { Box3, MOUSE, Vector3 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls as OrbitControlsProps } from 'three/examples/jsm/controls/OrbitControls';
 
 interface Props {
   /**
@@ -17,25 +18,7 @@ interface Props {
 
 export const PreviewModel: React.FC<Props> = (props) => {
   return (
-    <Canvas
-      camera={{
-        rotation: [0, 0, -Math.PI / 4],
-        fov: 90,
-        near: 0.1,
-        far: 1000,
-        position: [0, 30, 30],
-      }}
-    >
-      <OrbitControls
-        makeDefault
-        autoRotate
-        mouseButtons={{
-          LEFT: MOUSE.RIGHT,
-          RIGHT: MOUSE.LEFT,
-        }}
-      />
-      {/* <ambientLight intensity={0.1} /> */}
-      <pointLight position={[0, 50, 20]} />
+    <Canvas>
       <Sky />
       <Suspense
         fallback={
@@ -55,7 +38,49 @@ const Model: React.FC<Props> = ({ url, isSnapshot }) => {
     return null;
   }
 
-  const gltf = useLoader(GLTFLoader, url, (progress) => {});
+  const { scene } = useLoader(GLTFLoader, url, (progress) => {});
+  const { camera } = useThree();
+  const controlsRef = useRef<OrbitControlsProps>(null);
+  // 获取包围盒
+  const box = useMemo(() => new Box3().setFromObject(scene), [scene]);
 
-  return <primitive object={gltf.scene} />;
+  // 获取中心点作为控制器目标
+  const target = useMemo(
+    () => new Vector3().copy(box.getCenter(new Vector3())),
+    [box]
+  );
+
+  useEffect(() => {
+    if (!controlsRef.current) {
+      return;
+    }
+    // 计算相机与模型的距离
+    const distance = box.getSize(new Vector3()).length();
+    // 根据模型大小调整相机位置
+    const offset =
+      distance /
+      Math.tan(
+        (Math.PI / 180.0) * (camera as THREE.PerspectiveCamera).fov * 0.5
+      );
+
+    // 设置相机位置和目标点
+    controlsRef.current.target.copy(target);
+    controlsRef.current.object.position.set(
+      target.x,
+      target.y,
+      target.z + offset
+    );
+    controlsRef.current.update();
+  }, [box, camera, target]);
+
+  return (
+    <primitive object={scene}>
+      <OrbitControls
+        ref={controlsRef}
+        target={target}
+        enableDamping // 是否启用阻尼效果,默认为false
+        dampingFactor={0.1} // 适中的缓动效果,范围是0到1
+      />
+    </primitive>
+  );
 };
