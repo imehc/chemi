489793a7326961bbabc3f29c2e4f1786b0259a5c
imehc <richohealthy@hotmail.com>
2024-04-24 17:26:19 +0800

    fix: build错误

--- src/components/env-background/index.tsx
+++ src/components/env-background/index.tsx
@@ -1,9 +1,7 @@
 import { FC } from 'react';
 import { useEnvironment, Environment } from '@react-three/drei';
-import { useThree } from "@react-three/fiber";
 
 export const EnvBackground: FC = () => {
-  const { scene } = useThree()
   // https://github.com/pmndrs/drei?tab=readme-ov-file#useenvironment
   const envMap = useEnvironment({
     files: '/hdris/hochsal_field_1k.hdr',
