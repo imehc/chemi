0aa52849719b606075d8feb48be8a4fe0f252532
imehc <richohealthy@hotmail.com>
2024-05-30 17:19:50 +0800

    feat(browser): 添加速率限制

--- browser/middleware.ts
+++ browser/middleware.ts
@@ -1,9 +1,17 @@
 import createMiddleware from "next-intl/middleware";
 import { locales, defaultLocale } from "./config";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { RateLimiterMemory } from "rate-limiter-flexible";
 
 // TODO: https://github.com/nextauthjs/next-auth/issues/9536#issuecomment-1880180450
 // export { auth as middleware } from "～/auth"
+
+// Initialize the rate limiter
+// https://dev.to/sh20raj/how-to-add-rate-limiting-to-your-nextjs-app-router-22fa
+const rateLimiter = new RateLimiterMemory({
+  points: 10, // Number of points
+  duration: 1, // Per second
+})
 
 const intlMiddleware = createMiddleware({
   // A list of all locales that are supported
@@ -15,8 +23,15 @@ const intlMiddleware = createMiddleware({
 });
 
 export default async function middleware(req: NextRequest) {
-  // TODO: 处理auth
-  return intlMiddleware(req);
+  try {
+    // Consume a point for each request
+    await rateLimiter.consume(req.ip as string);
+    // TODO: 处理auth
+    return intlMiddleware(req);
+  } catch (error) {
+    // If rate limit is exceeded, send a 429 response
+    return new NextResponse('Too many requests', { status: 429 });
+  }
 }
 
 export const config = {
